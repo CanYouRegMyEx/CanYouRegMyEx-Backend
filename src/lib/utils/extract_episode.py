558a2d7af3f44b621586477bb4f1eadf4c7bf5b9
pppoh <2ndPipatpong987123@gmail.com>
2025-09-05 01:15:28 +0700

    create class MainCharacter, SideCharacter, Gadget, Case, Episode

---
+++ src/lib/utils/extract_episode.py
@@ -0,0 +1,135 @@
+import re
+
+class MainCharacter:
+    def __init__(
+            self,
+            character_url: str,
+            character_image_url: str,
+            name_eng: str,
+            ):
+        
+        self.character_url = character_url
+        self.character_image_url = character_image_url
+        self.name_eng = name_eng
+        
+    def to_dict(self):
+        return {
+            "character_url": self.character_url,
+            "character_image_url": self.character_image_url,
+            "name_eng": self.name_eng,
+        }
+
+class SideCharacter:
+    def __init__(
+            self,
+            character_image_url: str,
+            name_eng: str,  
+            description_character: list,
+            ) -> None:
+        self.character_image_url = character_image_url
+        self.name_eng = name_eng
+        self.description_character = description_character
+
+    
+    def to_dict(self):
+        return {
+            "character_image_url": self.character_image_url,
+            "name_eng": self.name_eng,
+            "description_character": self.description_character,
+        }
+
+class Gadget:
+    def __init__(
+            self,
+            gadget_url: str,
+            name_eng: str,
+            )-> None:
+        self.gadget_url = gadget_url
+        self.name_eng = name_eng
+    
+    def to_dict(self):
+        return {
+            "gadget_url": self.gadget_url,
+            "name_eng": self.name_eng,
+        }
+
+class Case:
+    def __init__(
+            self,
+            situation: str,
+            case_image_url: str,
+            crime_type: str,
+            location: str,
+            victims_name: str,
+            cause_of_death: str,
+            suspects_name_list: list,
+            crime_description: str
+
+            )-> None:
+        self.situation = situation
+        self.case_image_url = case_image_url
+        self.crime_type = crime_type
+        self.location = location
+        self.victims_name = victims_name
+        self.cause_of_death = cause_of_death
+        self.suspects_name_list = suspects_name_list
+        self.crime_description = crime_description
+
+
+    def to_dict(self):
+        return {
+            "situation": self.situation,
+            "case_image_url": self.case_image_url,
+            "crime_type": self.crime_type,
+            "location": self.location,
+            "victims_name": self.victims_name,
+            "suspects_name_list": self.suspects_name_list,
+            "crime_description": self.crime_description,
+        }
+
+class Episode:
+    def __init__(
+            self,
+            episode_number: str,
+            international_number: str,
+            title_eng: str,
+            title_kanji: str,
+            title_romaji: str,
+            description: str,
+            season: str,
+            airdate: str,
+            main_characters: list,
+            side_characters: list,
+            cases: list,
+            gadgets: list,
+    ) -> None:
+        self.episode_number = episode_number
+        self.international_number = international_number
+        self.title_eng = title_eng
+        self.title_kanji = title_kanji
+        self.title_romaji = title_romaji
+        self.description = description
+        self.season = season
+        self.airdate = airdate
+        self.main_characters = main_characters
+        self.side_characters = side_characters
+        self.cases = cases
+        self.gadgets = gadgets
+
+    def to_dict(self):
+        return {
+            "episode_number": self.episode_number,
+            "international_number": self.international_number,
+            "title_eng": self.title_eng,
+            "title_kanji": self.title_kanji,
+            "title_romaji": self.title_romaji,
+            "description": self.description,
+            "season": self.season,
+            "airdate": self.airdate,
+            "main_characters": self.main_characters,
+            "side_characters": self.side_characters,
+            "cases": self.cases,
+            "gadgets": self.gadgets,
+        }
+
+
