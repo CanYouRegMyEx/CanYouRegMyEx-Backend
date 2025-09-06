6555d09f9d9246ae267cbd6274b98ee7758f4f26
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2025-09-06 15:51:45 +0700

    example api endpoint

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
-from typing import Union
+from typing import Any, Union
 
 from fastapi import FastAPI
 from pydantic import BaseModel
+
+from lib.utils.extract_links import extract_links_asdict
 
 app = FastAPI()
 
@@ -18,10 +20,17 @@ def read_root():
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
+def read_item(item_id: int, q: Union[str, None] = None) -> dict[str, Any]:
     return {"item_id": item_id, "q": q}
 
 
 @app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
+def update_item(item_id: int, item: Item) -> dict[str, Any]:
     return {"item_name": item.name, "item_id": item_id}
+
+@app.get("/list")
+def get_list():
+    page = ''
+    with open('./Anime - Detective Conan Wiki.html', 'r') as f:
+        page = f.read()
+    return list(extract_links_asdict(page))
