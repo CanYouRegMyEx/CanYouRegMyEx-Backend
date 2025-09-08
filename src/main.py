3978bd0a23057f628c76649b50a7d0db5e82b53f
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2025-09-08 23:48:03 +0700

    fix: :coffin: whoops remove testing file and import

--- src/main.py
+++ src/main.py
@@ -3,14 +3,12 @@ from typing import Any, Union
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-from routers import episode_copy, episode_list
+from routers import episode_list
 from lib.utils.extract_character import extract_character, Character
 
 app = FastAPI()
 
 app.include_router(episode_list.router)
-app.include_router(episode_copy.router)
-
 
 class Item(BaseModel):
     name: str
