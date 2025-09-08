f66323783446822bf94cb233520e63d69e201cff
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2025-09-08 17:30:26 +0700

    feat: :sparkles: change singular season filter to list

--- src/routers/episode.py
+++ src/routers/episode.py
@@ -1,4 +1,4 @@
-from typing import Annotated
+from typing import Annotated, List
 from fastapi import APIRouter, Query
 from pydantic import BaseModel, Field
 from lib.utils.extract_links import Plot
@@ -7,9 +7,9 @@ from lib.utils.extract_links import Plot
 class FilterParams(BaseModel):
     model_config = {"extra": "forbid"}
 
-    filter: list[str] = Field([], max_length=10)
-    plot: list[Plot] = Field([], max_length=10)
-    season: int | None = Field(None, ge=1)
+    filter: List[str] = Field([], max_length=10)
+    plot: List[Plot] = Field([], max_length=10)
+    season: List[int] = Field([], max_length=10)
     limit: int = Field(100, gt=0, le=100)
     offset: int = Field(0, ge=0)
 
