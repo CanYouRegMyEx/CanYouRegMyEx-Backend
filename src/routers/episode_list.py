69e259071871a3113db121f28c04a712638213a0
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2025-09-17 18:31:33 +0700

    feat: :sparkles: seasons and plots endpoint

--- src/routers/episode_list.py
+++ src/routers/episode_list.py
@@ -1,7 +1,7 @@
 from typing import Annotated
 from fastapi import APIRouter, HTTPException, Query
 from lib.utils.crawler import crawl
-from lib.utils.extract_episode_list import FilterParams, extract_episodes
+from lib.utils.extract_episode_list import FilterParams, Plot, extract_episodes, extract_seasons
 
 
 EPISODES_PAGE_URL = 'https://www.detectiveconanworld.com/wiki/Anime'
@@ -16,3 +16,16 @@ async def get_episodes(filter_params: Annotated[FilterParams, Query()]):
     if not page:
         raise HTTPException(status_code=500, detail=f'Unable to crawl {EPISODES_PAGE_URL}')
     return extract_episodes(page, filter_params)
+
+
+@router.get('/metadata/seasons')
+async def get_metadata_seasons():
+    page = crawl(EPISODES_PAGE_URL)
+    if not page:
+        raise HTTPException(status_code=500, detail=f'Unable to crawl {EPISODES_PAGE_URL}')
+    return extract_seasons(page)
+
+
+@router.get('/metadata/plots')
+async def get_metadata_plots():
+    return list(map(lambda plot: plot.value, Plot))
